0ef5aa2a7656258af1804f5d8813ce880cf87b5a
Fernando Mariano Pereira <fernandovmp@outlook.com>
2020-11-20 16:27:25 -0300

    feat(backend): add migration to container entity

---
+++ src/TesteT2S.WebApi/Data/Migrations/20201120192334_Container.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TesteT2S.WebApi.Data.Migrations
+{
+    public partial class Container : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Containers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Number = table.Column<string>(type: "nvarchar(11)", maxLength: 11, nullable: false),
+                    Customer = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Type = table.Column<byte>(type: "tinyint", nullable: false),
+                    Status = table.Column<byte>(type: "tinyint", nullable: false),
+                    Category = table.Column<byte>(type: "tinyint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Containers", x => x.Id);
+                    table.UniqueConstraint("AK_Containers_Number", x => x.Number);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Containers");
+        }
+    }
+}
