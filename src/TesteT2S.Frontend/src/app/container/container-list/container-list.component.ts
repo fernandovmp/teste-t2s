978534fc6d217c99ec3a7daa3dbb576d059ee117
Fernando Mariano Pereira <fernandovmp@outlook.com>
2020-11-22 18:01:42 -0300

    feat(frontend): add sorting to containers list

--- src/TesteT2S.Frontend/src/app/container/container-list/container-list.component.ts
+++ src/TesteT2S.Frontend/src/app/container/container-list/container-list.component.ts
@@ -3,7 +3,7 @@ import { BehaviorSubject, Observable, of, Subject } from 'rxjs';
 import { map, switchMap } from 'rxjs/operators';
 import { IApiPaginatedResource } from 'src/app/types/api';
 import { Container } from '../Container';
-import { ContainerService } from '../container.service';
+import { ContainerService, IContainerServiceGetOptions } from '../container.service';
 
 @Component({
   selector: 'app-container-list',
@@ -15,13 +15,18 @@ export class ContainerListComponent implements OnInit {
   containers$: Observable<IApiPaginatedResource<Container>>
   pageIndex = new BehaviorSubject<number>(1)
   containers: IApiPaginatedResource<Container>
+  sortRule = "";
+  sortProperty = "";
 
   constructor(private containerService: ContainerService) { }
 
   ngOnInit(): void {
     this.containers$ = this.pageIndex.pipe(switchMap(page => {
-      console.log(page);
-      return this.containerService.getAll({ pagina: page });
+      let options: IContainerServiceGetOptions = { pagina: page }
+      if (this.sortProperty) {
+        options = { ...options, ordenarPor: `${this.sortProperty}_${this.sortRule}` }
+      }
+      return this.containerService.getAll(options);
     }));
     this.containers$.subscribe(data => console.log(data));
     this.containerService.getAll()
@@ -30,6 +35,22 @@ export class ContainerListComponent implements OnInit {
 
   changePage(pg: number) {
     this.pageIndex.next(pg);
+  }
+
+  sort(property: string, rule: "ascend" | "descend" | null) {
+    this.sortProperty = property;
+    if (rule === "ascend") {
+      this.sortRule = "asc";
+    }
+    else if (rule === "descend") {
+      this.sortRule = "desc";
+    }
+    else {
+      this.sortRule = null;
+      this.sortProperty = null;
+    }
+
+    this.pageIndex.next(this.pageIndex.value);
   }
 
 }
--- src/TesteT2S.Frontend/src/app/container/container.service.ts
+++ src/TesteT2S.Frontend/src/app/container/container.service.ts
@@ -4,9 +4,10 @@ import { environment } from '../../environments/environment';
 import { IApiPaginatedResource } from '../types/api';
 import { Container, PostContainerData } from './Container';
 
-interface IContainerServiceGetOptions {
+export interface IContainerServiceGetOptions {
   pagina?: number;
   tamanho?: number;
+  ordenarPor?: string;
 }
 
 @Injectable({
@@ -17,9 +18,12 @@ export class ContainerService {
   constructor(private http: HttpClient) { }
 
   getAll(options?: IContainerServiceGetOptions) {
-    const params = new HttpParams()
+    let params = new HttpParams()
       .append('pagina', String(options?.pagina ?? 1))
       .append('tamanho', String(options?.tamanho ?? 10));
+    if (options?.ordenarPor) {
+      params = params.append('ordenar_por', options.ordenarPor);
+    }
     return this.http.get<IApiPaginatedResource<Container>>(`${environment.apiUrl}/containers`, {
       params: params
     });
