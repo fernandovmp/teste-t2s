c158b8162aacc3dda3bc27ba49ea6316b713af55
Fernando Mariano Pereira <fernandovmp@outlook.com>
2020-11-23 01:04:15 -0300

    feat(backend): add validator to CreateHandlingViewModel

---
+++ src/TesteT2S.WebApi/Features/ShipHandling/Validations/CreateHandlingValidator.cs
@@ -0,0 +1,20 @@
+using FluentValidation;
+using TesteT2S.WebApi.Features.ShipHandling.ViewModels;
+
+namespace TesteT2S.WebApi.Features.ShipHandling.Validations
+{
+    public class CreateHandlingValidator : AbstractValidator<CreateHandlingViewModel>
+    {
+        public CreateHandlingValidator()
+        {
+            RuleFor(handling => handling.Navio)
+                .NotEmpty().WithMessage("Navio é obrigatório")
+                .MaximumLength(50).WithMessage("Navio não deve ultrapassar 50 caractéres");
+            RuleFor(handling => handling.TipoMovimentacao)
+                .IsInEnum().WithMessage("Valor do tipo de movimentação inválida");
+            RuleFor(handling => handling.DataFim)
+                .LessThan(handling => handling.DataFim)
+                .WithMessage("Data de inicio deve ser inferior a data de fim");
+        }
+    }
+}
